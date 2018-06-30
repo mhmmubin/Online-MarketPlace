59100d5fca8f0dfe8251f9e464b042bc3a9d6efd
mhmmubin <36401740+mhmmubin@users.noreply.github.com>
2018-06-30 19:24:12 +0930

    Google Location Map

---
+++ mymap.js
@@ -0,0 +1,6 @@
+ var map;
+      function initMap() 
+	  {
+        map = new google.maps.Map(document.getElementById('map'), {center: {lat: -34.763845, lng: 138.643993},
+        zoom: 15});
+      }
