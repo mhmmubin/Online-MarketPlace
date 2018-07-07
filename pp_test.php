8cc20647bbfd3059e1c71e53bf86887457a76ff8
Mobashsher Mubin <36401740+mhmmubin@users.noreply.github.com>
2018-07-07 11:45:50 +0930

    Update pp_test.php

--- pp_test.php
+++ pp_test.php
@@ -1,15 +1,12 @@
 <?php
 
-/*  PHP Paypal IPN Integration Class Demonstration File
- *  4.16.2005 - Micah Carrick, email@micahcarrick.com
-*/
 // Setup class
 require_once('pp.class.php');  // include the class file
 $p = new pp_class;             // initiate an instance of the class
 $p->paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';   // testing paypal url
 //$p->paypal_url = 'https://www.paypal.com/cgi-bin/webscr';     // paypal url
             
-// setup a variable for this script (ie: 'http://www.micahcarrick.com/paypal.php')
+// setup a variable for this script
 $this_script = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 
 // if there is not action variable, set the default action of 'process'
@@ -18,12 +15,6 @@ if (empty($_GET['action'])) $_GET['action'] = 'process';
 switch ($_GET['action']) {
     
    case 'process':      // Process and order...
- 
-      // For example, after ensureing all the POST variables from your custom
-      // order form are valid, you might have:
-      //
-      // $p->add_field('first_name', $_POST['first_name']);
-      // $p->add_field('last_name', $_POST['last_name']);
       
       $p->add_field('business', 'mhm.mubin@gmail.com');
       $p->add_field('return', $this_script.'?action=success');
@@ -36,49 +27,25 @@ switch ($_GET['action']) {
       //$p->dump_fields();      // for debugging, output a table of all the fields
       break;
       
-   case 'success':      // Order was successful...
-   
-      // This is where you would have the code to
-      // email an admin, update the database with payment status, activate a
-      // membership, etc.  
+   case 'success':      // Order was successful
  
       echo "<html><head><title>Success</title></head><body><h3>Thank you for your order.</h3>";
       foreach ($_POST as $key => $value) { echo "$key: $value<br>"; }
       echo "</body></html>";
-      
-      // You could also simply re-direct them to another page, or your own 
-      // order status page which presents the user with the status of their
-      // order based on a database
-	  
+    
       break;
       
-   case 'cancel':       // Order was canceled...
-
-      // The order was canceled before being completed.
- 
+   case 'cancel':       // Order was canceled
+		
       echo "<html><head><title>Canceled</title></head><body><h3>The order was canceled.</h3>";
       echo "</body></html>";
       
       break;
       
-   case 'ipn':          // Paypal is calling page for IPN validation...
-   
-      // It's important to remember that paypal calling this script.  There
-      // is no output here.  This is where you validate the IPN data and if it's
-      // valid, update your database to signify that the user has payed.  If
-      // you try and use an echo or printf function here it's not going to do you
-      // a bit of good.  This is on the "backend".  That is why, by default, the
-      // class logs all IPN data to a text file.
-      
+   case 'ipn':          
+		
       if ($p->validate_ipn()) {
-          
-         // Payment has been recieved and IPN is verified.  This is where you
-         // update your database to activate or process the order, or setup
-         // the database with the user's order details, email an administrator,
-         // etc.  You can access a slew of information via the ipn_data() array.
-  
-  
-         // For this example, we'll just email ourselves ALL the data.
+         
          $subject = 'Instant Payment Notification - Recieved Payment';
          $to = 'mhm.mubin@gmail.com';    //  your email
          $body =  "An instant payment notification was successfully recieved\n";
