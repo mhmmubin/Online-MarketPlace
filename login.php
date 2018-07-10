96744b25411aad5466cbbeb342e1c4a3ede4c037
Mobashsher Mubin <36401740+mhmmubin@users.noreply.github.com>
2018-07-10 18:35:01 +0930

    Update login.php

--- login.php
+++ login.php
@@ -1,39 +1,4 @@
- <!DOCTYPE html>
- 
- <html lang="en">
- <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-     
-    <title>Wood-Lore</title>
-	 
-	<link rel="shortcut icon" href="images/tree.ico" />
-	 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		
-	 <link href="css/ecomm.css" rel="stylesheet"/>
-		
-	<link href="css/animate.min.css" rel="stylesheet">
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		
-	 
-</head>
-     <body class="loginphp_body">
-        <nav role="navigation" class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-					<a class="navbar-brand" href="home.html"><img src="images/wood.png" class="logo"></a>
-					<button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse" >  
-						   <span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-					</button>
-                </div>
-     
+   
    <?php
 //start the new session
 session_start();
